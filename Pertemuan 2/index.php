c45492dbc6ef8826b9d52187c97ed3e0316c9190
dikkydz <kontakaphrodite@gmail.com>
2021-02-11 14:58:32 +0700

    menyelesaikan video pertemuan 2

---
+++ Pertemuan 2/index.php
@@ -0,0 +1,67 @@
+<?php
+/*
+Dikky Dzikriansyah
+143040035
+https://github.com/dikkydz/pw2021_143040035
+Pertemuan 2 - 8 Februari 2021
+Mempelajari mengenai sintaks PHP
+*/
+?>
+
+<?php
+
+// Pertemuan 2 - PHP Dasar
+// sintaks PHP
+
+// Standar Output
+// echo, print
+// print_r
+// var_dump
+
+// Penulisan sintaks PHP
+// 1. PHP di dalam HTML
+// 2. HTML didalam PHP
+
+// Variabel dan Tipe Data
+// variabel
+// tidak boleh diawali dengan angka, tapi boleh mengandung angka
+
+// $nama = "Dikky Dzikriansyah";
+// echo 'Halo, nama saya $nama';
+
+// Operator
+// aritmatika
+//  +, -, *, /, %
+// $x = 10;
+// $y = 20;
+// echo $x * $y;
+
+// penggabung string / concatenation / concat
+// .
+// $nama_depan = "Dikky";
+// $nama_belakang = "Dzikriansyah";
+// echo $nama_depan . " " . $nama_belakang;
+
+// Assignment
+// =, +=, -=, *=, /=, %=, .=
+// $x = 1;
+// $x -= 5;
+// echo $x;
+
+// $nama = "Dikky";
+// $nama .= " ";
+// $nama .= "Dzikriansyah";
+// echo $nama;
+
+// Perbandingan
+// <, >, <=, >=, ==
+// var_dump(1 == "1");
+
+// Identitas
+// ===, !==
+// var_dump(1 === "1")
+
+// Logika
+// &&, ||, !
+// $x = 30;
+// var_dump($x < 20 || $x % 2 == 0);
