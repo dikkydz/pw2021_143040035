649f84f5883c0abd2f235a1f156535b2ef21f67a
dikkydz <kontakaphrodite@gmail.com>
2021-02-18 21:54:35 +0700

    menyelesaikan video pertemuan 3

---
+++ Pertemuan 3/latihan2.php
@@ -0,0 +1,29 @@
+<?php
+/*
+Dikky Dzikriansyah
+143040035
+https://github.com/dikkydz/pw2021_143040035
+Pertemuan 3 - 18 Februari 2021
+Mempelajari mengenai Pengkondisian / Percabangan
+*/
+?>
+
+
+<?php
+
+// Pengkondisian / Percabangan
+// if else
+// if else if else
+// ternary
+// switch
+
+// $x = 20;
+// if ( $x < 20 ) {
+//     echo "benar";
+// } else if ( $x == 20 ) {
+//     echo "bingo!";
+// } else {
+//     echo "salah";
+// }
+    
+?>
